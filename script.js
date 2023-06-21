48ce26d5bacf6a836ca05b2c5d0ad5fd29a0a488
nav571 <128248831+Nav571@users.noreply.github.com>
2023-06-21 19:41:46 -0400

    Added functions to allowgrid size selection

--- script.js
+++ script.js
@@ -7,6 +7,9 @@ let colorPicker=document.getElementById('colorpicker');
 
 let color = 'black';
 
+inputbtn.addEventListener('click', changeGrid); 
+
+
 function createGrid (size) {
     gridbox.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
     gridbox.style.gridTemplateRows= `repeat(${size}, 1fr)`;
@@ -15,11 +18,46 @@ function createGrid (size) {
     
     for (let i=0; i <area; i++) {
         let square = document.createElement("div");
-        square.addEventListener('mouseover', () => {
-            square.style.backgroundColor='black'
-        })       
+        square.addEventListener('mouseover', changeColor)
         square.style.backgroundColor= "white";
         gridbox.appendChild(square);
     }
 }
 createGrid(16);
+
+function removeAllChildNodes(parent){
+    while(parent.firstChild){
+        parent.removeChild(parent.firstChild);
+    }
+}
+function changeGrid (size) {
+    removeAllChildNodes(gridbox);
+    size= getSize();  
+    gridbox.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    gridbox.style.gridTemplateRows= `repeat(${size}, 1fr)`;
+
+    let area= size*size
+    
+    for (let i=0; i <area; i++) {
+        let square = document.createElement("div");
+        square.addEventListener('mouseover', changeColor);       
+        square.style.backgroundColor= "white";
+        gridbox.appendChild(square);
+    }
+}
+function getSize (input) {
+    input=parseInt(prompt("Please enter a number between 2-100"));
+    if (input <= 100 && input >= 2) {
+        return(input);
+    } else if (input >100) {
+        console.log("Number entered is greater than 100");
+    } else if (input <2) {
+        return "Number entered is less than 2";
+    } else {
+        return "Please enter a number between 2-100";
+    }
+}
+function changeColor () {
+    this.style.backgroundColor= color;
+    
+}
